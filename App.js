88d87e2e4f06b84f0ba282481ed3c34fda5c7d3f
Pahmi Alifya Bahri <pahmi1998@gmail.com>
2021-11-15 22:11:39 +0700

    config

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { NativeBaseProvider } from "native-base";
+import StackNavigator from "./navigation/StackNavigator";
 
 export default function App() {
+  // useEffect(() => {
+  //   store.dispatch({ type: commonActionTypes.HIEDE_LOADING });
+  // }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <StackNavigator />
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
