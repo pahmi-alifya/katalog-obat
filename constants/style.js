5776bfc2fd315b33554c957db59970620e26b567
Pahmi Alifya Bahri <pahmi1998@gmail.com>
2021-11-15 22:11:34 +0700

    constant

---
+++ constants/style.js
@@ -0,0 +1,32 @@
+export const Colors = {
+  primary: "#54BBE0",
+  secondary: "#F0C349",
+  white: "#ffffff",
+  dark: "#000000",
+  card: "#d6ceb9",
+  danger: "#f53333",
+  textSecondary: "#EFB61D",
+  textPrimary: "#2FB5E1",
+};
+
+export const ShadowSm = {
+  shadowColor: `${Colors.semiDark}`,
+  shadowRadius: 10,
+  shadowOpacity: 0.6,
+  elevation: 8,
+  shadowOffset: {
+    width: 0,
+    height: 7,
+  },
+};
+
+export const Shadow = {
+  shadowColor: `${Colors.dark}`,
+  shadowOffset: {
+    width: 0,
+    height: 12,
+  },
+  shadowOpacity: 0.58,
+  shadowRadius: 16.0,
+  elevation: 24,
+};
