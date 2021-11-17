88f298ba37fb69e44d29a30e09a64796f737d915
Pahmi Alifya Bahri <pahmi1998@gmail.com>
2021-11-17 21:58:40 +0700

    update navigation

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -7,15 +7,15 @@ import LoginScreen from "../screens/LoginScreen";
 import MedicineDetailScreen from "../screens/MedicineDetailScreen";
 import MedicineGroupScreen from "../screens/MedicineGroupScreen";
 import ProfileScreen from "../screens/ProfileScreen";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 // import api from "../api";
 // import upload from "../api/upload";
 
 const RootStack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  // const isLoggedIn = useSelector((state) => state.auth.token);
-  // const user = useSelector((state) => state.auth);
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  const user = useSelector((state) => state.user);
 
   // if (isLoggedIn) {
   //   api.defaults.headers.common = {
@@ -27,47 +27,31 @@ const StackNavigator = () => {
   // }
   return (
     <RootStack.Navigator headerMode="none" initialRouteName="Login">
-      <RootStack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen name="Profile" component={ProfileScreen} />
-      <RootStack.Screen name="Content" component={ContentScreen} />
-      <RootStack.Screen name="MedicineGroup" component={MedicineGroupScreen} />
-      <RootStack.Screen
-        name="MedicineDetail"
-        component={MedicineDetailScreen}
-      />
-      {/* {isLoggedIn && user.length === undefined && user.user !== "" ? (
-        <RootStack.Screen
-          name="App"
-          component={DrawerNavigator}
-          options={{
-            animationEnabled: false,
-          }}
-        />
+      {isLoggedIn && user && user?.data !== "" ? (
+        <>
+          <RootStack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false, headerLeft: () => null }}
+          />
+          <RootStack.Screen name="Profile" component={ProfileScreen} />
+          <RootStack.Screen name="Content" component={ContentScreen} />
+          <RootStack.Screen
+            name="MedicineGroup"
+            component={MedicineGroupScreen}
+          />
+          <RootStack.Screen
+            name="MedicineDetail"
+            component={MedicineDetailScreen}
+          />
+        </>
       ) : (
-        // <RootStack.Screen
-        //   name="Auth"
-        //   component={AuthStackNavigator}
-        //   options={{
-        //     animationEnabled: false,
-        //   }}
-        // />
         <RootStack.Screen
-          name="Auth"
-          component={AuthStackNavigator}
-          options={{
-            animationEnabled: false,
-          }}
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
         />
-      )} */}
+      )}
     </RootStack.Navigator>
   );
 };
