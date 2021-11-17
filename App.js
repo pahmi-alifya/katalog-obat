f79584ca8c87ea602250a41eca4641a7170ae95e
Pahmi Alifya Bahri <pahmi1998@gmail.com>
2021-11-17 21:59:57 +0700

    update redux

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
 import StackNavigator from "./navigation/StackNavigator";
 
+import { persistor, store } from "./redux";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { commonActionTypes } from "./redux/actions/common";
+
 export default function App() {
-  // useEffect(() => {
-  //   store.dispatch({ type: commonActionTypes.HIEDE_LOADING });
-  // }, []);
+  useEffect(() => {
+    store.dispatch({ type: commonActionTypes.HIEDE_LOADING });
+  }, []);
   return (
-    <NavigationContainer>
-      <NativeBaseProvider>
-        <StackNavigator />
-      </NativeBaseProvider>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <NativeBaseProvider>
+            <StackNavigator />
+          </NativeBaseProvider>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
